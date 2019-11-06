144daaa4eaee8776d34d26f99dcc70fd5b88ee42
shivaswaroop <shivaswaroops2@gmail.com>
2019-11-06 15:14:55 -0800

    hibernate examples

---
+++ hibernate_project/src/main/java/com/lti/hibernate_ex/hibernate_project/Employee.java
@@ -0,0 +1,26 @@
+package com.lti.hibernate_ex.hibernate_project;
+
+public class Employee {
+	private int id;
+	private String name;
+	private String branch;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+
+}
